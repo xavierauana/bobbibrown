138f0fbbb3e7cf0243b20c92be3af53d122b8201
Xavier Au <xavier.au@anacreation.com>
2017-08-15 20:18:47 +0800

    updated app icons and title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
     <link rel="apple-touch-icon" sizes="180x180" href="/files/icons/App_Icon_2x.png">
     <link rel="apple-touch-icon" sizes="167x167" href="/files/icons/App_Icon_2x.png">
 	
