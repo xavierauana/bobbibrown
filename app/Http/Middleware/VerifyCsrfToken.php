73d0317504f042d1f23edd57353b6379b2462073
Xavier Au <xavier.au@anacreation.com>
2018-01-11 17:36:05 +0800

    update csrf exception

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,8 @@ class VerifyCsrfToken extends BaseVerifier
      * @var array
      */
     protected $except = [
-        //
+        "/login",
+        "/admin/login",
+        "/logout"
     ];
 }
